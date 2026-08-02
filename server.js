2dfdda833252da1d1a9d2b7328baefd668638f78
ShiMu-art <shimu@read-along.dev>
2026-08-02 04:35:49 +0000

    诊断第一步：只加载store模块

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-console.log("HELLO FROM NODE");
-console.error("HELLO FROM STDERR");
+console.error("[1] START");
 var http = require("http");
+console.error("[2] HTTP LOADED");
+var store = require("./lib/store");
+console.error("[3] STORE LOADED, DATA_DIR=" + store.DATA_DIR);
 var PORT = process.env.PORT || 3000;
 var server = http.createServer(function(req, res) {
   res.writeHead(200, {"Content-Type": "text/plain"});
