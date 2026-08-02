dca7cbe12e04519b16ea0731a92a7529585a6645
ShiMu-art <shimu@read-along.dev>
2026-08-02 04:38:33 +0000

    诊断第二步：加载所有模块(push/epub/txt/import)

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 console.error("[1] START");
 var http = require("http");
-console.error("[2] HTTP LOADED");
+console.error("[2] HTTP");
 var store = require("./lib/store");
-console.error("[3] STORE LOADED, DATA_DIR=" + store.DATA_DIR);
+console.error("[3] STORE");
+var pushMod = require("./lib/push");
+console.error("[4] PUSH enabled=" + pushMod.PUSH_ENABLED);
+var epub = require("./lib/epub");
+console.error("[5] EPUB");
+var txt = require("./lib/txt");
+console.error("[6] TXT");
+var imp = require("./lib/import");
+console.error("[7] IMPORT");
 var PORT = process.env.PORT || 3000;
 var server = http.createServer(function(req, res) {
   res.writeHead(200, {"Content-Type": "text/plain"});
